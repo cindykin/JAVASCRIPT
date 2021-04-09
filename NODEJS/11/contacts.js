f8e8ce0e0c1335567e2ff3b37a5b5939410b1bdb
kinvictor <kinvictorwin@gmail.com>
2021-04-09 16:09:09 +0700

    fitur 11 done

--- NODEJS/11/contacts.js
+++ NODEJS/11/contacts.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const clog = console.log
 const chalk = require('chalk')
 const validator = require('validator')
+const chwarnstyle = chalk.red.inverse.bold
 
 const dirPath = './data'
 if(!fs.existsSync(dirPath)) {
@@ -24,13 +25,23 @@ const simpanKontak = (nama, notelp, email) => {
     // cek duplikat
     const duplikat = users.find((user) => user.nama === nama)
     if (duplikat) {
-        clog(chalk.red.inverse.bold(`sudah ada kontak yang terdaftar atas nama ${nama}, mohon ketikkan nama lain !`))
+        clog(chwarnstyle(`sudah ada kontak yang terdaftar atas nama ${nama}, mohon ketikkan nama lain !`))
+        return false
+    }
+
+    // cek notelp
+    if(!validator.isMobilePhone(notelp, 'any')) {
+        clog(chwarnstyle('nomor yang anda masukkan bukan merupakan format nomor yang benar!'))
         return false
     }
 
     // cek email
-    // if(!validator.isEmail(email)){
-        // clog(chalk.red.inverse.bold('email yang anda kirim bukan format email yang benar, mohon cek ulang email anda !'))}
+    if(email) {
+        if(!validator.isEmail(email)){
+            clog(chwarnstyle('email yang anda kirim bukan format email yang benar, mohon cek ulang email anda !'))
+            return false
+        }
+    }
 
     users.push(user)
 
